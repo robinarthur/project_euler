df882824b1272300a8997c785b7fdb4cab77170f
robinarthur <stukka@gmx.de>
2018-05-08 10:48:11 +0200

    upd

--- #5.py
+++ #5.py
@@ -8,6 +8,11 @@
 
 div = [x for x in range(1,21)]
 
+l = []
+
 for i in range(20, 10000):
     for e in div:
-        if i % e 
+        l.append(i % e == 0 )
+    if all(item == True for item in l):
+        print(l)
+        print(i)
