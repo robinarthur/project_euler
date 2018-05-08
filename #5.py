94a353add4312d6bee841974942d896422aaf459
robinarthur <stukka@gmx.de>
2018-05-08 13:25:51 +0200

    upd #5

--- #5.py
+++ #5.py
@@ -8,11 +8,19 @@
 
 div = [x for x in range(1,21)]
 
-l = []
+start = 100000000
+step = 10000
+k, j = start, 0
+stop = 1000000000000
 
-for i in range(20, 10000):
+for i in range(start, stop):
+    l = []
     for e in div:
         l.append(i % e == 0 )
+    j += 1
+    if j % step == 0:
+        k += step
+        print('checked :', k)
     if all(item == True for item in l):
         print(l)
         print(i)
