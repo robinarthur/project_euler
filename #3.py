0ed25a93871296303586f9940f0c299a0a7b07ac
robinarthur <stukka@gmx.de>
2018-05-08 20:25:56 +0200

    #3

--- #3.py
+++ #3.py
@@ -4,4 +4,30 @@
 # What is the largest prime factor of the number 600851475143 ?
 #______________________________________________________________________________
 
+def is_prime(n):
+    if n <= 1: return False
+    if n == 2 or n == 3: return True # filter out number 2 and 3
+    if n % 2 == 0 or n < 2: return False
+    
+    for i in range(3, int(n**0.5)+1,2): # only odd numbers
+        if n % i == 0: return False
+    return True
 
+i = number = 600851475143
+l = []
+k = 0
+
+while l[-1] >= 0:
+    i -= 1
+    if is_prime(i):
+        l.append(i)
+        
+        # check if the prime is a factor of number
+        
+        
+    if i % 100000 == 0:
+        k += 100000
+        print('checked: ', k)
+    
+print("yuhuu")
+print(l[-1])
