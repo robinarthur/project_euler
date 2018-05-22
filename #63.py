fb50cbab2371c13a4e5fc2c96ed8c382c6826a58
robinarthur <stukka@gmx.de>
2018-05-22 06:59:48 +0200

    edit #63

---
+++ #63.py
@@ -0,0 +1,35 @@
+# Problem #63:
+# 
+# The 5-digit number, 16807=7^5, is also a fifth power. Similarly, the 9-digit
+# number, 134217728=8^9, is a ninth power.
+#
+# How many n-digit positive integers exist which are also an nth power?
+#______________________________________________________________________________
+
+import math
+
+def is_power(number):
+    l = len(number)
+    if isinstance(ln(number) / ln(l), int):
+        return True
+
+
+
+
+
+
+    
+#  print(math.exp(exponent*math.log(base))) 
+# isinstance(4, int)
+
+
+
+
+
+# 16807 = x^5  5=len
+
+# ln(16807) = x * ln(len)
+
+# ln(16807 / ln(len) = x
+
+# if isistance(x, int) return 
