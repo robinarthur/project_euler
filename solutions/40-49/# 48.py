2781aa61e5491981329b9f51867c529f80cf4e5b
robinarthur <stukka@gmx.de>
2020-05-04 07:49:29 +0200

    Update # 48.py

--- solutions/40-49/# 48.py
+++ solutions/40-49/# 48.py
@@ -1,3 +1,8 @@
+# selfpower
+# The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+#
+# Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+
 S=0
 for i in range(1,1001):
 s+=pow(i,i)
