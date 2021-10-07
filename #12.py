af91a3be4c689c66462e7893d2ca6661317e22a1
robinarthur <frey0000r@gmail.com>
2021-10-07 17:21:38 +0200

    Update #12.py

--- #12.py
+++ #12.py
@@ -40,17 +40,50 @@ We can see that 28 is the first triangle number to have over five divisors.
 What is the value of the first triangle number to have over five hundred divisors?
 
 """
+import time
+from math import sqrt
 
-def find_divisors(n):
-    list_of_divisors = []
-    for i in range(1,n+1):
-        if n % i == 0:
-            list_of_divisors.append(n)
-    return list_of_divisors
+def count_divisors(n):
+    d = {}
+    count = 1
     
-
-for i in range(1,10000000):
-    if len(find_divisors(i)) > 499:
-        print(i)
-        break
+    while n % 2 == 0:
+        n = n / 2
+        try:
+            d[2] += 1
+        except KeyError:
+            d[2] = 1
             
+    for i in range(3, int(sqrt(n+1)), 2):
+        while n % i == 0 and i != n:
+            n = n / i
+            try:
+                d[i] += 1
+            except KeyError:
+                d[i] = 1
+    
+    d[n] = 1
+    
+    for _,v in d.items():
+        count = count * (v +1)
+    
+    return count
+
+
+def tri_number(num):
+    next = 1 + int(sqrt(1+(8 * num)))
+    return num + (next/2)
+
+
+def main():
+    i = 1
+    while count_divisors(i) < 500:
+        i = tri_number(i)
+    return i
+
+
+start = time.time()
+answer = main()
+elapsed = (time.time() - start)
+
+print("result %s returned in %s seconds." % (answer, elapsed))
