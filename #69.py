dfea3f79ab777efe410adcb258bf28fe4f12a5b5
robinarthur <frey0000r@gmail.com>
2021-11-30 12:34:49 +0100

    should work

---
+++ #69.py
@@ -0,0 +1,46 @@
+"""
+Euler's Totient function, φ(n) [sometimes called the phi function], is used to determine the number of numbers less than n which are relatively prime to n. For example, as 1, 2, 4, 5, 7, and 8, are all less than nine and relatively prime to nine, φ(9)=6.
+
+n	Relatively Prime	φ(n)	n/φ(n)
+2	1	1	2
+3	1,2	2	1.5
+4	1,3	2	2
+5	1,2,3,4	4	1.25
+6	1,5	2	3
+7	1,2,3,4,5,6	6	1.1666...
+8	1,3,5,7	4	2
+9	1,2,4,5,7,8	6	1.5
+10	1,3,7,9	4	2.5
+It can be seen that n=6 produces a maximum n/φ(n) for n ≤ 10.
+
+Find the value of n ≤ 1,000,000 for which n/φ(n) is a maximum.
+"""
+
+def gcd(p,q):
+# Create the gcd of two positive integers.
+    while q != 0:
+        p, q = q, p%q
+    return p
+
+def is_coprime(x, y):
+    return gcd(x, y) == 1
+
+def phi_func(x):
+    if x == 1:
+        return 1
+    else:
+        n = [y for y in range(1,x) if is_coprime(x,y)]
+        return len(n)
+
+rslt = []
+
+for n in range(1,1000001):
+    liste = []
+    totient = phi_func(n)  #totient
+    div = int(n / totient)
+    liste = div, totient, n
+    rslt.append(liste)
+v = max(rslt)
+
+print('div, totient, n')
+print(max(rslt))
